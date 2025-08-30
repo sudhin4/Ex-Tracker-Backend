25583b0b119f137352173ddf995f3ef5b021ca64
sudhin4 <sudhinnaveen38@gmail.com>
2025-08-30 20:52:37 +0530

    Some changes

--- VerifyToken.js
+++ VerifyToken.js
@@ -7,40 +7,20 @@ const secertkey = process.env.SECERT_KEY;
 
 async function verifyToken(req, res, next) {
 
-    const token = req.cookies.Ts;
 
-
-    if (!token) {
-        res.json({
-            valid: false,
-            message: 'Token Not found'
-        })
+    try {
+        const token = req.cookies.Ts;
+        if (!token) return res.status(401).json({ message: "No token" });
+        const payload = jwt.verify(token, process.env.SECRET_KEY);
+        req.id = payload.email;
+        next();
     }
-    else {
-        try {
-            const verifytkn = jwt.verify(token, secertkey);
-
-            const tokenemail = verifytkn.email;
-
-
-            const isemailindb = await signupmodel.findOne({ Email: tokenemail })
-
-
-            if (isemailindb) {
-                req.id = isemailindb.Email;
-
-            }
-            else {
-                req.id = null
-            }
-            next();
-        } catch (error) {
-            return res.status(401).json({
-                success: false,
-                message: "Invalid Token"
-            })
-        }
+    catch (error) {
+        console.log("Error in verify token", error);
+        res.status(401).json({ message: "Invalid token" });
     }
+
+
 
 
 }
