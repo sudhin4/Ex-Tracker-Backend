1449e693b56fbf5b1c3c4ebcce34126aec197e62
sudhin4 <sudhinnaveen38@gmail.com>
2025-09-01 22:39:49 +0530

    Cors change

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(cookieParser())
 
 app.use(cors({
   origin: [
-    "https://extracker-ten.vercel.app/",                
+    "https://extracker-ten.vercel.app",                
   ],
   methods: ["GET", "POST", "PUT", "DELETE","HEAD","PATCH"],
   credentials: true
